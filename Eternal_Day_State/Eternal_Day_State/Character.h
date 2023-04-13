5f125b4808301fcc4bc791574dfa39dd133d77db
neil616 <nnn60106@gmail.com>
2023-04-13 21:29:09 +0800

    renew 角色 struct (4/13)

--- Eternal_Day_State/Eternal_Day_State/Character.h
+++ Eternal_Day_State/Eternal_Day_State/Character.h
@@ -1,4 +1,19 @@
-//¨¤¦â¼ÒªO
+#pragma once
+//Struct that you input the statue to create a character
+struct Statue
+{
+	int Str;
+	int Con;
+	int Dex;
+	int Pow;
+	int App;
+	int Int;
+	int Siz;
+	int Edu;
+	int Luk;
+};
+
+//The actually character that create from the statue that you input
 struct Character
 {
 	int Str;
@@ -15,3 +30,6 @@ struct Character
 	int San;
 	int DB;
 };
+
+//The function that to create a character
+Character Create_Character(Statue);
