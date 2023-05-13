2a54599a156ff3c1a37d839ef4f46c35be452792
neil616 <nnn60106@gmail.com>
2023-05-13 17:37:05 +0800

    5/13(basic screen

--- Eternal_Day_State/Eternal_Day_State/Form1.h
+++ Eternal_Day_State/Eternal_Day_State/Form1.h
@@ -49,6 +49,28 @@ namespace CppCLRWinFormsProject {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Button^ start;
+	protected:
+	private: System::Windows::Forms::Button^ manual;
+	private: System::Windows::Forms::TextBox^ manual_text;
+	private: System::Windows::Forms::Button^ back_to_home;
+	private: System::Windows::Forms::Label^ Game_title;
+	private: System::Windows::Forms::Button^ archaeologist_button;
+	private: System::Windows::Forms::Button^ reporter_button;
+	private: System::Windows::Forms::Button^ occultist_button;
+	private: System::Windows::Forms::Label^ chose_character_text;
+	private: System::Windows::Forms::Button^ backpack;
+	private: System::Windows::Forms::Panel^ backpack_panel;
+	private: System::Windows::Forms::Panel^ status_panel;
+	private: System::Windows::Forms::Label^ STR;
+	private: System::Windows::Forms::Label^ character_name;
+	private: System::Windows::Forms::Label^ player_power_text;
+
+
+
+
+
+
 
 	protected:
 
@@ -65,20 +87,310 @@ namespace CppCLRWinFormsProject {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->start = (gcnew System::Windows::Forms::Button());
+			this->manual = (gcnew System::Windows::Forms::Button());
+			this->manual_text = (gcnew System::Windows::Forms::TextBox());
+			this->back_to_home = (gcnew System::Windows::Forms::Button());
+			this->Game_title = (gcnew System::Windows::Forms::Label());
+			this->archaeologist_button = (gcnew System::Windows::Forms::Button());
+			this->reporter_button = (gcnew System::Windows::Forms::Button());
+			this->occultist_button = (gcnew System::Windows::Forms::Button());
+			this->chose_character_text = (gcnew System::Windows::Forms::Label());
+			this->backpack = (gcnew System::Windows::Forms::Button());
+			this->backpack_panel = (gcnew System::Windows::Forms::Panel());
+			this->status_panel = (gcnew System::Windows::Forms::Panel());
+			this->player_power_text = (gcnew System::Windows::Forms::Label());
+			this->character_name = (gcnew System::Windows::Forms::Label());
+			this->STR = (gcnew System::Windows::Forms::Label());
+			this->backpack_panel->SuspendLayout();
+			this->status_panel->SuspendLayout();
 			this->SuspendLayout();
+			// 
+			// start
+			// 
+			this->start->Font = (gcnew System::Drawing::Font(L"Times New Roman", 9, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->start->Location = System::Drawing::Point(756, 565);
+			this->start->Name = L"start";
+			this->start->Size = System::Drawing::Size(134, 46);
+			this->start->TabIndex = 0;
+			this->start->Text = L"START";
+			this->start->UseVisualStyleBackColor = true;
+			this->start->Click += gcnew System::EventHandler(this, &Form1::start_Click);
+			// 
+			// manual
+			// 
+			this->manual->Font = (gcnew System::Drawing::Font(L"Times New Roman", 9, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->manual->Location = System::Drawing::Point(344, 565);
+			this->manual->Name = L"manual";
+			this->manual->Size = System::Drawing::Size(161, 46);
+			this->manual->TabIndex = 1;
+			this->manual->Text = L"MANUAL";
+			this->manual->UseVisualStyleBackColor = true;
+			this->manual->Click += gcnew System::EventHandler(this, &Form1::manual_Click);
+			// 
+			// manual_text
+			// 
+			this->manual_text->Location = System::Drawing::Point(-1, 0);
+			this->manual_text->Multiline = true;
+			this->manual_text->Name = L"manual_text";
+			this->manual_text->ReadOnly = true;
+			this->manual_text->Size = System::Drawing::Size(1201, 530);
+			this->manual_text->TabIndex = 2;
+			this->manual_text->Visible = false;
+			// 
+			// back_to_home
+			// 
+			this->back_to_home->Font = (gcnew System::Drawing::Font(L"Times New Roman", 9, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->back_to_home->Location = System::Drawing::Point(292, 565);
+			this->back_to_home->Name = L"back_to_home";
+			this->back_to_home->Size = System::Drawing::Size(252, 45);
+			this->back_to_home->TabIndex = 3;
+			this->back_to_home->Text = L"Back to Home";
+			this->back_to_home->UseVisualStyleBackColor = true;
+			this->back_to_home->Visible = false;
+			this->back_to_home->Click += gcnew System::EventHandler(this, &Form1::back_to_home_Click);
+			// 
+			// Game_title
+			// 
+			this->Game_title->AutoSize = true;
+			this->Game_title->Font = (gcnew System::Drawing::Font(L"標楷體", 36, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(136)));
+			this->Game_title->Location = System::Drawing::Point(340, 233);
+			this->Game_title->Name = L"Game_title";
+			this->Game_title->Size = System::Drawing::Size(525, 96);
+			this->Game_title->TabIndex = 4;
+			this->Game_title->Text = L"永晝城物語";
+			// 
+			// archaeologist_button
+			// 
+			this->archaeologist_button->Font = (gcnew System::Drawing::Font(L"標楷體", 9, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(136)));
+			this->archaeologist_button->Location = System::Drawing::Point(267, 378);
+			this->archaeologist_button->Name = L"archaeologist_button";
+			this->archaeologist_button->Size = System::Drawing::Size(142, 54);
+			this->archaeologist_button->TabIndex = 5;
+			this->archaeologist_button->Text = L"考古學家";
+			this->archaeologist_button->UseVisualStyleBackColor = true;
+			this->archaeologist_button->Visible = false;
+			this->archaeologist_button->Click += gcnew System::EventHandler(this, &Form1::archaeologist_button_Click);
+			// 
+			// reporter_button
+			// 
+			this->reporter_button->Font = (gcnew System::Drawing::Font(L"標楷體", 9, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(136)));
+			this->reporter_button->Location = System::Drawing::Point(788, 378);
+			this->reporter_button->Name = L"reporter_button";
+			this->reporter_button->Size = System::Drawing::Size(143, 54);
+			this->reporter_button->TabIndex = 6;
+			this->reporter_button->Text = L"記者";
+			this->reporter_button->UseVisualStyleBackColor = true;
+			this->reporter_button->Visible = false;
+			this->reporter_button->Click += gcnew System::EventHandler(this, &Form1::reporter_button_Click);
+			// 
+			// occultist_button
+			// 
+			this->occultist_button->Font = (gcnew System::Drawing::Font(L"標楷體", 9, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(136)));
+			this->occultist_button->Location = System::Drawing::Point(526, 378);
+			this->occultist_button->Name = L"occultist_button";
+			this->occultist_button->Size = System::Drawing::Size(143, 54);
+			this->occultist_button->TabIndex = 7;
+			this->occultist_button->Text = L"神祕學家";
+			this->occultist_button->UseVisualStyleBackColor = true;
+			this->occultist_button->Visible = false;
+			this->occultist_button->Click += gcnew System::EventHandler(this, &Form1::occultist_button_Click);
+			// 
+			// chose_character_text
+			// 
+			this->chose_character_text->AutoSize = true;
+			this->chose_character_text->Font = (gcnew System::Drawing::Font(L"標楷體", 36, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(136)));
+			this->chose_character_text->Location = System::Drawing::Point(212, 233);
+			this->chose_character_text->Name = L"chose_character_text";
+			this->chose_character_text->Size = System::Drawing::Size(719, 96);
+			this->chose_character_text->TabIndex = 8;
+			this->chose_character_text->Text = L"請選擇開局角色";
+			this->chose_character_text->Visible = false;
+			// 
+			// backpack
+			// 
+			this->backpack->Font = (gcnew System::Drawing::Font(L"標楷體", 9, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(136)));
+			this->backpack->Location = System::Drawing::Point(0, 3);
+			this->backpack->Name = L"backpack";
+			this->backpack->Size = System::Drawing::Size(141, 51);
+			this->backpack->TabIndex = 9;
+			this->backpack->Text = L"背包";
+			this->backpack->UseVisualStyleBackColor = true;
+			this->backpack->Visible = false;
+			this->backpack->Click += gcnew System::EventHandler(this, &Form1::backpack_Click);
+			// 
+			// backpack_panel
+			// 
+			this->backpack_panel->Controls->Add(this->backpack);
+			this->backpack_panel->Location = System::Drawing::Point(-1, 617);
+			this->backpack_panel->Name = L"backpack_panel";
+			this->backpack_panel->Size = System::Drawing::Size(1201, 57);
+			this->backpack_panel->TabIndex = 10;
+			this->backpack_panel->Visible = false;
+			// 
+			// status_panel
+			// 
+			this->status_panel->Controls->Add(this->player_power_text);
+			this->status_panel->Controls->Add(this->STR);
+			this->status_panel->Location = System::Drawing::Point(-1, 0);
+			this->status_panel->Name = L"status_panel";
+			this->status_panel->Size = System::Drawing::Size(1201, 76);
+			this->status_panel->TabIndex = 11;
+			this->status_panel->Visible = false;
+			// 
+			// player_power_text
+			// 
+			this->player_power_text->AutoSize = true;
+			this->player_power_text->Font = (gcnew System::Drawing::Font(L"標楷體", 9, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(136)));
+			this->player_power_text->Location = System::Drawing::Point(6, 7);
+			this->player_power_text->Name = L"player_power_text";
+			this->player_power_text->Size = System::Drawing::Size(148, 24);
+			this->player_power_text->TabIndex = 2;
+			this->player_power_text->Text = L"角色能力值:";
+			this->player_power_text->Visible = false;
+			// 
+			// character_name
+			// 
+			this->character_name->AutoSize = true;
+			this->character_name->Font = (gcnew System::Drawing::Font(L"標楷體", 9, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(136)));
+			this->character_name->Location = System::Drawing::Point(2, 590);
+			this->character_name->Name = L"character_name";
+			this->character_name->Size = System::Drawing::Size(185, 24);
+			this->character_name->TabIndex = 1;
+			this->character_name->Text = L"選擇的角色職業";
+			this->character_name->Visible = false;
+			// 
+			// STR
+			// 
+			this->STR->AutoSize = true;
+			this->STR->Font = (gcnew System::Drawing::Font(L"Times New Roman", 9, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->STR->Location = System::Drawing::Point(170, 5);
+			this->STR->Name = L"STR";
+			this->STR->Size = System::Drawing::Size(57, 26);
+			this->STR->TabIndex = 0;
+			this->STR->Text = L"STR";
+			this->STR->Visible = false;
 			// 
 			// Form1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(13, 24);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(1000, 1000);
+			this->ClientSize = System::Drawing::Size(1200, 675);
+			this->Controls->Add(this->status_panel);
+			this->Controls->Add(this->character_name);
+			this->Controls->Add(this->backpack_panel);
+			this->Controls->Add(this->chose_character_text);
+			this->Controls->Add(this->occultist_button);
+			this->Controls->Add(this->reporter_button);
+			this->Controls->Add(this->archaeologist_button);
+			this->Controls->Add(this->Game_title);
+			this->Controls->Add(this->back_to_home);
+			this->Controls->Add(this->manual_text);
+			this->Controls->Add(this->manual);
+			this->Controls->Add(this->start);
+			this->Margin = System::Windows::Forms::Padding(4);
 			this->Name = L"Form1";
-			this->Text = L"Form1";
+			this->Text = L"COC";
+			this->Load += gcnew System::EventHandler(this, &Form1::Form1_Load);
+			this->backpack_panel->ResumeLayout(false);
+			this->status_panel->ResumeLayout(false);
+			this->status_panel->PerformLayout();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
 	private: System::Void backgroundWorker1_DoWork(System::Object^ sender, System::ComponentModel::DoWorkEventArgs^ e) {
 	}
-	};
+	private: System::Void Form1_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void manual_Click(System::Object^ sender, System::EventArgs^ e) {
+		//buttons
+		manual->Visible = false;
+		manual_text->Visible = true;
+		back_to_home->Visible = true;
+		//text
+		Game_title->Visible = false;
+
+	}
+	private: System::Void back_to_home_Click(System::Object^ sender, System::EventArgs^ e) {
+		//buttons
+		manual->Visible = true;
+		manual_text->Visible = false;
+		back_to_home->Visible = false;
+		//text
+		Game_title->Visible = true;
+	}
+	private: System::Void start_Click(System::Object^ sender, System::EventArgs^ e) {
+		//all object visable false
+		manual->Visible = false;
+		manual_text->Visible = false;
+		back_to_home->Visible = false;
+		Game_title->Visible = false;
+		start->Visible = false;
+		//create character page visable
+		archaeologist_button->Visible = true;
+		occultist_button->Visible = true;
+		reporter_button->Visible = true;
+		chose_character_text->Visible = true;
+	}
+	private: System::Void archaeologist_button_Click(System::Object^ sender, System::EventArgs^ e) {
+		archaeologist_button->Visible = false;
+		occultist_button->Visible = false;
+		reporter_button->Visible = false;
+		chose_character_text->Visible = false;
+		//change to scene one
+		backpack_panel->Visible = true;
+		backpack->Visible = true;
+		STR->Visible = true;
+		status_panel->Visible = true;
+		character_name->Text = L"考古學家";
+		character_name->Visible = true;
+		player_power_text->Visible = true;
+	}
+	private: System::Void occultist_button_Click(System::Object^ sender, System::EventArgs^ e) {
+		archaeologist_button->Visible = false;
+		occultist_button->Visible = false;
+		reporter_button->Visible = false;
+		chose_character_text->Visible = false;
+		//change to scene one
+		backpack_panel->Visible = true;
+		backpack->Visible = true;
+		STR->Visible = true;
+		status_panel->Visible = true;
+		character_name->Text = L"神祕學家";
+		character_name->Visible = true;
+		player_power_text->Visible = true;
+	}
+	private: System::Void reporter_button_Click(System::Object^ sender, System::EventArgs^ e) {
+		archaeologist_button->Visible = false;
+		occultist_button->Visible = false;
+		reporter_button->Visible = false;
+		chose_character_text->Visible = false;
+		//change to scene one
+		backpack_panel->Visible = true;
+		backpack->Visible = true;
+		STR->Visible = true;
+		status_panel->Visible = true;
+		character_name->Text = L"記者";
+		character_name->Visible = true;
+		player_power_text->Visible = true;
+	}
+	private: System::Void backpack_Click(System::Object^ sender, System::EventArgs^ e) {
+		//backpack object display
+
+	}
+};
 }
